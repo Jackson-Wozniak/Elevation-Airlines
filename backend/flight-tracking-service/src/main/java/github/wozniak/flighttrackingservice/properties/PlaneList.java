c36ab19ff174bcb23006c9f91b04352f6c10f7f8
Jackson-Wozniak <jjw30@comcast.net>
2023-07-09 12:55:17 -0400

    randomly choose planes for fleet based on likelyhood instead of totally random

--- backend/flight-tracking-service/src/main/java/github/wozniak/flighttrackingservice/properties/PlaneList.java
+++ backend/flight-tracking-service/src/main/java/github/wozniak/flighttrackingservice/properties/PlaneList.java
@@ -15,8 +15,14 @@ public class PlaneList {
 
     private static final Random random = new Random();
     private static final List<PlaneModel> planeModels = List.of(
+            //~40% of fleet
             new PlaneModel("B737", 189, 20, 449, 3500),
-            new PlaneModel("B787", 246, 40, 492, 8786)
+            //~20% of fleet
+            new PlaneModel("B787", 246, 40, 492, 8786),
+            //~20% of fleet
+            new PlaneModel("B757", 220, 30, 459, 4488),
+            //~20% of fleet
+            new PlaneModel("A320neo", 194, 20, 453, 3500)
     );
 
     public static List<Plane> getDefaultPlanes(){
@@ -26,7 +32,7 @@ public class PlaneList {
         for(int i = 0; i < 30; i++){
             int callSign = nums.get(random.nextInt(nums.size()));
             nums.remove(Integer.valueOf(callSign));
-            defaultPlanes.add(new Plane("ELV" + callSign, planeModels.get(random.nextInt(planeModels.size()))));
+            defaultPlanes.add(new Plane("ELV" + callSign, randomPlane()));
         }
         return defaultPlanes;
     }
@@ -38,9 +44,24 @@ public class PlaneList {
         for(int i = 0; i < planes; i++){
             int callSign = nums.get(random.nextInt(nums.size()));
             nums.remove(Integer.valueOf(callSign));
-            defaultPlanes.add(new Plane("ELV" + callSign, planeModels.get(random.nextInt(planeModels.size()))));
+            defaultPlanes.add(new Plane("ELV" + callSign, randomPlane()));
         }
         return defaultPlanes;
+    }
+
+    private static PlaneModel randomPlane(){
+        int percent = random.nextInt(100);
+        if(percent < 40){
+            return planeModels.get(0);
+        }
+        if(percent < 60){
+            return planeModels.get(1);
+        }
+        if(percent < 80){
+            return planeModels.get(2);
+        }
+        return planeModels.get(3);
+
     }
 
     @AllArgsConstructor
