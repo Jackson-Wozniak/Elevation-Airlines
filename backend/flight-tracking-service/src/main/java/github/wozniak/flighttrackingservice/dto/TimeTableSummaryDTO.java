9a81a330b249d6ce97d5a49234c7ae4fccab388d
Jackson-Wozniak <jjw30@comcast.net>
2023-07-09 13:09:23 -0400

    only send necessary flight info for certain endpoints that return larger lists of flight. This allows for lower response sizes while still returning all relevant information

---
+++ backend/flight-tracking-service/src/main/java/github/wozniak/flighttrackingservice/dto/TimeTableSummaryDTO.java
@@ -0,0 +1,23 @@
+package github.wozniak.flighttrackingservice.dto;
+
+import github.wozniak.flighttrackingservice.model.FlightTimeTable;
+import github.wozniak.flighttrackingservice.utils.DateTimeUtils;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class TimeTableSummaryDTO {
+    private String date;
+    private int flightCount;
+    private List<FlightSummaryDTO> flights;
+
+    public TimeTableSummaryDTO(FlightTimeTable timeTable){
+        this.date = DateTimeUtils.format(timeTable.getDate());
+        this.flights = timeTable.getFlightsToday().stream().map(FlightSummaryDTO::new).toList();
+        this.flightCount = flights.size();
+    }
+}
