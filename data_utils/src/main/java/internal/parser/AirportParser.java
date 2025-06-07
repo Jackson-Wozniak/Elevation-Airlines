24178df7d1a6fb9027399e3ed0bedd5db1e1eaa1
Jackson Wozniak <jjw30@comcast.net>
2025-06-07 17:30:42 -0400

    add Runway file parser

--- data_utils/src/main/java/internal/parser/AirportParser.java
+++ data_utils/src/main/java/internal/parser/AirportParser.java
@@ -1,13 +1,27 @@
 package internal.parser;
 
 import internal.parser.files.PassengerCSVFile;
+import internal.parser.files.RunwayCSVFile;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class AirportParser {
     public static void main( String[] args ) throws IOException {
         PassengerCSVFile file = PassengerCSVFile.getInstance();
 
-        System.out.println(file.getPassengersPerAirport());
+        Map<String, Integer> passengers = file.getPassengersPerAirport();
+        RunwayCSVFile file2 = RunwayCSVFile.getInstance();
+
+        AtomicInteger count = new AtomicInteger(0);
+
+        passengers.forEach((code, passengerCount) -> {
+            if(file2.getRunwaysPerAirport().containsKey("K" + code)) count.incrementAndGet();
+            System.out.println(code + ": " + passengerCount);
+        });
+        System.out.println(file2.getRunwaysPerAirport().size());
+        System.out.println(passengers.size());
+        System.out.println(count.get());
     }
 }
--- data_utils/src/main/java/internal/parser/files/RunwayCSVFile.java
+++ data_utils/src/main/java/internal/parser/files/RunwayCSVFile.java
@@ -1,7 +1,76 @@
 package internal.parser.files;
 
+import internal.parser.objects.RunwayInfo;
+import internal.parser.utils.CSVReaderUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /*
 Stores a map of all runways from runways.csv in resources folder
  */
 public class RunwayCSVFile {
+    private static RunwayCSVFile runwayCSVFile;
+    private static final String FILE_PATH = "unparsed_data/runways.csv";
+    private static final int MINIMUM_RUNWAY_LENGTH = 4000;
+
+    private Map<String, Integer> runwaysPerAirport = new HashMap<>();
+
+    private RunwayCSVFile() throws IOException {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(FILE_PATH);
+        if(inputStream == null) throw new IOException("Cannot load class");
+
+        InputStreamReader runwayReader = new InputStreamReader(inputStream);
+
+        List<String[]> allLines = toArray(new BufferedReader(runwayReader).lines().toList());
+
+        Map<String, ArrayList<RunwayInfo>> possibleRunways = new HashMap<>();
+        for (String[] allLine : allLines) {
+            String code = allLine[0];
+            String runwayLengthStr = allLine[1];
+            int runwayLength = CSVReaderUtils.safeIntegerParse(runwayLengthStr);
+            String surface = allLine[2];
+            boolean hasLights = allLine[3].equals("1");
+            boolean isClosed = allLine[4].equals("1");
+
+            if(!possibleRunways.containsKey(code)){
+                possibleRunways.put(code, new ArrayList<>());
+            }
+            possibleRunways.get(code).add(new RunwayInfo(runwayLength, surface, hasLights, isClosed));
+        }
+        addAllowedAirports(possibleRunways);
+    }
+
+    //ensure at least one runway fits requirements for airport to be allowed in network
+    private void addAllowedAirports(Map<String, ArrayList<RunwayInfo>> possibleRunways){
+        possibleRunways.forEach((code, runwayInfos) -> {
+            int maxLength = runwayInfos.stream()
+                    .filter(runway -> runway.hasLights() && !runway.isClosed())
+                    .mapToInt(RunwayInfo::getLengthFt)
+                    .max().orElse(0);
+
+            if(maxLength >= MINIMUM_RUNWAY_LENGTH) runwaysPerAirport.put(code, maxLength);
+        });
+    }
+
+    private static List<String[]> toArray(List<String> strings){
+        return strings.stream().map(str -> str.split(",")).toList();
+    }
+
+    public static RunwayCSVFile getInstance() throws IOException {
+        if(runwayCSVFile == null){
+            runwayCSVFile = new RunwayCSVFile();
+        }
+        return runwayCSVFile;
+    }
+
+    public Map<String, Integer> getRunwaysPerAirport(){
+        return runwaysPerAirport;
+    }
 }
