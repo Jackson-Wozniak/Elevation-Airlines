6257141b70ea0d6a3ab1707b6b9addd41665f354
Jackson Wozniak <jjw30@comcast.net>
2025-06-16 18:54:06 -0400

    output county GDP and CAGR %

--- data_utils/src/main/java/internal/parser/AirportParser.java
+++ data_utils/src/main/java/internal/parser/AirportParser.java
@@ -12,42 +12,56 @@ import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class AirportParser {
-    private static final String OUTPUT_PATH = "./data_utils/output/airports.csv";
+    private static final String AIRPORTS_OUTPUT = "./data_utils/output/airports.csv";
+    private static final String COUNTY_ECONOMICS_OUTPUT = "./data_utils/output/county_economics.csv";
 
     public static void main( String[] args ) throws IOException {
         Map<String, Integer> passengers = PassengerCSVFile.getInstance().getPassengersPerAirport();
         Map<String, Integer> runways = RunwayCSVFile.getInstance().getRunwaysPerAirport();
         List<AirportInfo> airports = AirportCSVFile.getInstance().getAirports();
-        Map<String, CountyEconomicInfo> countyEconomicInfo = CountyEconomicsCSVFile.getInstance().getCounties();
         Map<String, CityInfo> cityInfo = CitiesCountiesCSVFile.getInstance().getCities();
         Map<String, Map<String, CityAviationStats>> cityAviationStats = CityAviationStatsCSVFile.getInstance().getCityStats();
 
+        List<AirportOutput> validAirports = new ArrayList<>();
+        airports.forEach(airport -> {
+            if(!airport.getCountry().equalsIgnoreCase("United States")) return;
+            if(!runways.containsKey(airport.getCode())) return;
 
-//        List<AirportOutput> validAirports = new ArrayList<>();
-//
-//        airports.forEach(airport -> {
-//            if(!runways.containsKey(airport.getCode())) return;
-//
-//            int passengerCount = 0;
-//            if(passengers.containsKey(airport.getCode())){
-//                passengerCount = passengers.get(airport.getCode());
-//            }else if(passengers.containsKey(airport.getLocalCode())){
-//                passengerCount = passengers.get(airport.getLocalCode());
-//            }
-//
-//            validAirports.add(new AirportOutput(airport, runways.get(airport.getCode()), passengerCount));
-//        });
-//
-//        writeAirportsToOutput(validAirports);
+            int passengerCount = 0;
+            if(passengers.containsKey(airport.getCode())){
+                passengerCount = passengers.get(airport.getCode());
+            }else if(passengers.containsKey(airport.getLocalCode())){
+                passengerCount = passengers.get(airport.getLocalCode());
+            }
+
+            validAirports.add(new AirportOutput(airport, runways.get(airport.getCode()), passengerCount));
+        });
+
+        writeAirportsToOutput(validAirports);
+
+        writeCountiesToOutput();
     }
 
     private static void writeAirportsToOutput(List<AirportOutput> airports){
-        try (FileWriter writer = new FileWriter(OUTPUT_PATH)) {
+        try (FileWriter writer = new FileWriter(AIRPORTS_OUTPUT)) {
             writer.write("Code,Size,Name,Latitude,Longitude,Continent,Country,Region,City,Runway Length (FT),Passengers-Per-Year\n");
             for(AirportOutput airport : airports){
                 writer.write(airport.toString());
             }
-            System.out.println("Airports written successfully to " + OUTPUT_PATH);
+            System.out.println("Airports written successfully to " + AIRPORTS_OUTPUT);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public static void writeCountiesToOutput() throws IOException {
+        Map<String, CountyEconomicInfo> countyEconomicInfo = CountyEconomicsCSVFile.getInstance().getCounties();
+        try (FileWriter writer = new FileWriter(COUNTY_ECONOMICS_OUTPUT)) {
+            writer.write("County, State, GDP ($), CAGR %\n");
+            for(CountyEconomicInfo county : countyEconomicInfo.values()){
+                writer.write(county.toString());
+            }
+            System.out.println("County Economics written successfully to " + COUNTY_ECONOMICS_OUTPUT);
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
--- data_utils/src/main/java/internal/parser/objects/CountyEconomicInfo.java
+++ data_utils/src/main/java/internal/parser/objects/CountyEconomicInfo.java
@@ -1,18 +1,36 @@
 package internal.parser.objects;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class CountyEconomicInfo {
-    private String county;
-    private String state;
-    private String unit;
-    private String[] gdpValues2001To2023;
+    private final String county;
+    private final String state;
+    private final String unit;
+    private final String[] gdpValues2001To2023;
+    private double CAGR;
 
     public CountyEconomicInfo(String county, String state, String unit, String[] gdpValues2001To2023) {
         this.county = county;
         this.state = state;
         this.unit = unit;
         this.gdpValues2001To2023 = gdpValues2001To2023;
+        setCAGR();
+    }
+
+    private void setCAGR(){
+        List<Double> values = new ArrayList<>();
+        for(String gdp : gdpValues2001To2023){
+            try{
+                double value = Double.parseDouble(gdp);
+                values.add(value);
+            }catch (Exception ignored) { }
+        }
+
+        double rate = (Math.pow(values.get(values.size() - 1) / values.get(0), (1.0 / values.size()))) - 1;
+
+        this.CAGR = Math.round((rate * 100.0) * 100.00) / 100.00;
     }
 
     public String getCounty() {
@@ -31,9 +49,17 @@ public class CountyEconomicInfo {
         return gdpValues2001To2023;
     }
 
+    public double getCAGR(){
+        return CAGR;
+    }
+
     @Override
     public String toString(){
-        return county + "," + state + "," + unit + "," + Arrays.toString(gdpValues2001To2023);
+        long value = -1;
+        try{
+            value = (Long.parseLong(gdpValues2001To2023[gdpValues2001To2023.length - 1]) * 1000);
+        }catch (Exception ignored) { }
+        return county + "," + state + "," + value + "," + CAGR + "\n";
     }
 
     public String getKey(){
