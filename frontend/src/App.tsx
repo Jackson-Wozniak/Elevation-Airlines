bc2f7ecc961258251462f90b7b3474c6fad04320
Jackson Wozniak <jjw30@comcast.net>
2025-06-28 22:16:47 -0400

    add more pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import DocumentationPage from "./pages/DocumentationPage";
 import './styles/shared.css'
 import FlightDashboard from "./pages/FlightDashboard";
+import FlightDetailsPage from "./pages/FlightDetailsPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<FlightDashboard />} />
+      <Route path="flights/:flightId" element={<FlightDetailsPage />} />
       <Route path="docs" element={<DocumentationPage />} />
     </Routes>
   )
