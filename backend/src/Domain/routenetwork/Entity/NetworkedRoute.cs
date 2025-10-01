ce5b8102d5dcbfe58662dfec81dc4ff57db72359
Jackson Wozniak <jjw30@comcast.net>
2025-10-01 18:16:00 -0400

    add RouteMarketType to NetworkedRoute

--- backend/src/Domain/routenetwork/Entity/NetworkedRoute.cs
+++ backend/src/Domain/routenetwork/Entity/NetworkedRoute.cs
@@ -1,4 +1,5 @@
 ﻿using backend.Core.Entity;
+using backend.Domain.routenetwork.Enum;
 using Route = backend.Domain.shared.Route;
 
 namespace backend.Domain.routenetwork.Entity;
@@ -6,4 +7,6 @@ namespace backend.Domain.routenetwork.Entity;
 public class NetworkedRoute : BaseEntity
 {
     public Route Route { get; set; }
+    public int PlannedWeeklyFlights { get; set; }
+    public RouteMarketType MarketType { get; set; }
 }
---
+++ backend/src/Domain/routenetwork/Enum/RouteMarketType.cs
@@ -0,0 +1,24 @@
+﻿namespace backend.Domain.routenetwork.Enum;
+
+public enum RouteMarketType
+{
+    HubToMajorMarket = 1,
+    HubToSecondaryMarket = 2,
+    ReturnToHub = 3,
+    ConnectMajorMarkets = 4,
+    ConnectSecondaryMarkets = 5
+}
+
+public static class RouteMarketTypeUtils
+{
+    public static bool ArrivesAtHub(this RouteMarketType type)
+    {
+        return type == RouteMarketType.ReturnToHub;
+    }
+
+    public static bool DepartsFromHub(this RouteMarketType type)
+    {
+        return type is RouteMarketType.HubToMajorMarket
+            or RouteMarketType.HubToSecondaryMarket;
+    }
+}
