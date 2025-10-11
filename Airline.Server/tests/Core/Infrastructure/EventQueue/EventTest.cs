802150226f1bb963b8759405234cca592977baa1
Jackson Wozniak <jjw30@comcast.net>
2025-10-11 18:48:26 -0400

    add tests project

---
+++ Airline.Server/tests/Core/Infrastructure/EventQueue/EventTest.cs
@@ -0,0 +1,8 @@
+﻿using Airline.Server.Core.Infrastructure.EventQueue;
+
+namespace tests.Core.Infrastructure.EventQueue;
+
+public class EventTest : Event
+{
+    
+}
---
+++ Airline.Server/tests/Core/Infrastructure/EventQueue/PriorityEventQueueTests.cs
@@ -0,0 +1,25 @@
+﻿using Airline.Server.Core.Infrastructure.EventQueue;
+
+namespace tests.Core.Infrastructure.EventQueue;
+
+public class PriorityEventQueueTests
+{
+    [Fact]
+    public void Enqueue_OneInsert_CountIs1()
+    {
+        var queue = new PriorityEventQueue<EventTest>();
+        queue.Enqueue(new EventTest{ScheduledTime = DateTime.Now});
+        Assert.Equal(1, queue.Count());
+    }
+    
+    [Fact]
+    public void Enqueue_IncrementalInsert_CountIsN()
+    {
+        var queue = new PriorityEventQueue<EventTest>();
+        for (var i = 1; i <= 100; i++)
+        {
+            queue.Enqueue(new EventTest{ScheduledTime = DateTime.Now});
+            Assert.Equal(i, queue.Count());
+        }
+    }
+}
